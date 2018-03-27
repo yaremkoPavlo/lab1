9891f2074f6c0445792f25a134f680cc7d4eb8c3
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-03-27 22:29:16 +0300

    organize files

---
+++ lab1/flover.php
@@ -0,0 +1,25 @@
+<?php
+  class Flover {
+    private $name;
+    private $price;
+    private $avaible;
+
+    public function __construct($name, $price) {
+      $this->name = $name;
+      $this->price = $price;
+      $this->avaible = true;
+    }
+
+    public function setAvaible($av_bool) {
+      $this->avaible = $av_bool;
+    }
+
+    public function getAvaible() {
+      return $this->avaible;
+    }
+
+    public function getPrice() {
+      return $this->price;
+    }
+  }
+?>
